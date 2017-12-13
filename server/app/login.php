a8193b4362f9f268dff5fde346ff0ff5d9fe303b
soloproyectos <gonzalo.chumillas@gmail.com>
2017-12-13 22:14:20 +0100

    restructure

--- server/app/login.php
+++ server/app/login.php
@@ -1,6 +1,6 @@
 <?php
 header("Content-Type: application/json; charset=utf-8");
-require_once "vendor/autoload.php";
+require_once "src/vendor/autoload.php";
 require_once "config.php";
 use petitphotobox\controllers\LoginController;
 
