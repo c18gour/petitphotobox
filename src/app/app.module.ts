465fc82b85770cf4c673c1c6d93aef04eb1358d5
soloproyectos <gonzalo.chumillas@gmail.com>
2017-12-15 13:28:20 +0100

    generate views

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SingupComponent } from './singup/singup.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    SingupComponent
   ],
   imports: [
     BrowserModule
