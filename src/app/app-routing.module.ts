0a07933aa5e1c1618e2cd5c02495f761bc183da3
soloproyectos <gonzalo.chumillas@gmail.com>
2017-12-20 14:55:26 +0100

    rename component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { HomeComponent } from './components/home/home.component';
+import { HomeComponent } from './components/home/home-component';
 import { LoginComponent } from './components/login/login.component';
 import { SinginComponent } from './components/singin/singin.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
