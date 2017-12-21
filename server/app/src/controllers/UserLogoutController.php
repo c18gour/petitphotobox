4080e7cdf38a579ff14b42df22add7d39465bb2f
soloproyectos <gonzalo.chumillas@gmail.com>
2017-12-21 23:55:41 +0100

    logout controller

--- server/app/src/controllers/UserLogoutController.php
+++ server/app/src/controllers/UserLogoutController.php
@@ -1,9 +1,11 @@
 <?php
 namespace petitphotobox\controllers;
 use petitphotobox\core\controller\AuthController;
+use petitphotobox\documents\UserLogoutDocument;
 
 class UserLogoutController extends AuthController
 {
+  private $_document;
 
   /**
    * Creates a new instance.
@@ -11,7 +13,18 @@ class UserLogoutController extends AuthController
   public function __construct()
   {
     parent::__construct();
+    $this->_document = new UserLogoutDocument();
     $this->addPostRequestHandler([$this, "onPostRequest"]);
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * @return UserLoginDocument
+   */
+  public function getDocument()
+  {
+    return $this->_document;
   }
 
   /**
---
+++ server/app/src/documents/UserLogoutDocument.php
@@ -0,0 +1,8 @@
+<?php
+namespace petitphotobox\documents;
+use petitphotobox\core\model\document\BaseDocument;
+
+class UserLogoutDocument extends BaseDocument
+{
+
+}
