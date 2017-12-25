2958ffe3d454cfc2b7bb5d66b3f87721aeb51724
soloproyectos <gonzalo.chumillas@gmail.com>
2017-12-25 16:40:17 +0100

    inline todo

--- src/app/core/service/controller/base-controller.ts
+++ src/app/core/service/controller/base-controller.ts
@@ -3,6 +3,7 @@ import { BaseDocument } from '../../../core/model/document/base-document';
 export abstract class BaseController {
   constructor(public url: string) { }
 
+  // TODO: 'get()' and 'post()' should be optionals
   abstract async get(...params: any[]): Promise<BaseDocument>;
   abstract async post(...params: any[]): Promise<BaseDocument>;
 }
