c8e6fd9a71e27a25b8df4e9501a44465edc11e02
soloproyectos <gonzalo.chumillas@gmail.com>
2017-12-30 12:07:45 +0100

    autofocus

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AutofocusModule } from 'angular-autofocus-fix';
 
 import { AppRoutingModule } from './app-routing-module';
 import { AppComponent } from './app-component';
@@ -42,6 +43,7 @@ import { HomeController } from './controllers/home-controller';
   ],
   imports: [
     BrowserModule,
+    AutofocusModule,
     FormsModule,
     HttpModule,
     AppRoutingModule,
