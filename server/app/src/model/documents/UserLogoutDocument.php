d5b3685c6b5a78c59c9d21100c94688a1767f90a
soloproyectos <gonzalo.chumillas@gmail.com>
2018-01-02 14:10:04 +0100

    codefix

--- server/app/src/model/documents/UserLogoutDocument.php
+++ server/app/src/model/documents/UserLogoutDocument.php
@@ -2,7 +2,16 @@
 namespace petitphotobox\model\documents;
 use petitphotobox\core\model\document\BaseDocument;
 
+// TODO: remove this empty document (already exists one empty document)
 class UserLogoutDocument extends BaseDocument
 {
-
+  /**
+   * {@inheritdoc}
+   *
+   * @return object
+   */
+  protected function getJsonObject()
+  {
+    return [];
+  }
 }
