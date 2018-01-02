2c3bf6f51a07a0198577c2220f415005b0f5c452
soloproyectos <gonzalo.chumillas@gmail.com>
2018-01-02 14:10:37 +0100

    remove inline todo

--- src/app/entities/category-entity.ts
+++ src/app/entities/category-entity.ts
@@ -1,7 +1,6 @@
 import { BaseEntity } from '../core/entity/base-entity';
 import { InputTreeOptionEntity } from './input-tree-option-entity';
 
-// TODO: rename by CategoryEntity
 export class CategoryEntity extends BaseEntity {
   readonly id: string;
   title: string;
