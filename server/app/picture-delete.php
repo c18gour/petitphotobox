3d54483093acc8f19a74c127d8119aaffc5c097e
soloproyectos <gonzalo.chumillas@gmail.com>
2018-01-04 19:13:59 +0100

    remove controller

--- server/app/picture-delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-header("Content-Type: application/json; charset=utf-8");
-require_once "src/vendor/autoload.php";
-require_once "app-config.php";
-require_once "config.php";
-use petitphotobox\controllers\PictureDeleteController;
-
-$c = new PictureDeleteController();
-$c->processRequest();
