1992dd7bf729630466117a3ad05c3fb812046956
soloproyectos <gonzalo.chumillas@gmail.com>
2018-01-06 13:07:33 +0100

    recode

--- server/app/src/controllers/UserLogoutController.php
+++ server/app/src/controllers/UserLogoutController.php
@@ -2,40 +2,16 @@
 namespace petitphotobox\controllers;
 use petitphotobox\core\auth\UserAuth;
 use petitphotobox\core\controller\AuthController;
-use petitphotobox\model\documents\EmptyDocument;
 
 class UserLogoutController extends AuthController
 {
-  private $_document;
-
   /**
    * Creates a new instance.
    */
   public function __construct()
   {
     parent::__construct();
-    $this->addOpenRequestHandler([$this, "onOpenRequest"]);
     $this->addPostRequestHandler([$this, "onPostRequest"]);
-  }
-
-  /**
-   * {@inheritdoc}
-   *
-   * @return EmptyDocument
-   */
-  public function getDocument()
-  {
-    return $this->_document;
-  }
-
-  /**
-   * Processes OPEN requests.
-   *
-   * @return void
-   */
-  public function onOpenRequest()
-  {
-    $this->_document = new EmptyDocument();
   }
 
   /**
