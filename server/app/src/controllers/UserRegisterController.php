d89ed0794f14992e2eb39f2482c6e73b808ed2d8
soloproyectos <gonzalo.chumillas@gmail.com>
2018-01-06 13:18:32 +0100

    recode

--- server/app/src/controllers/UserRegisterController.php
+++ server/app/src/controllers/UserRegisterController.php
@@ -3,14 +3,14 @@ namespace petitphotobox\controllers;
 use petitphotobox\core\auth\UserAuth;
 use petitphotobox\core\controller\Controller;
 use petitphotobox\core\exception\ClientException;
+use petitphotobox\core\model\Document;
 use petitphotobox\exceptions\AccessDeniedError;
-use petitphotobox\model\documents\UserRegisterDocument;
 use petitphotobox\model\records\DbUser;
 use soloproyectos\text\Text;
 
 class UserRegisterController extends Controller
 {
-  private $_document;
+  private $_username = "";
 
   /**
    * Creates a new instance.
@@ -25,11 +25,15 @@ class UserRegisterController extends Controller
   /**
    * {@inheritdoc}
    *
-   * @return UserRegisterDocument
+   * @return Document
    */
   public function getDocument()
   {
-    return $this->_document;
+    return new Document(
+      [
+        "username" => $this->_username
+      ]
+    );
   }
 
   /**
@@ -39,17 +43,10 @@ class UserRegisterController extends Controller
    */
   public function onOpenRequest()
   {
-    $username = $this->getParam("username", "");
-    $password = $this->getParam("password");
-    $rePassword = $this->getParam("rePassword");
-
     if (UserAuth::isLogged($this->db)) {
+      // TODO: shouldn't it be a SessionError?
       throw new AccessDeniedError("The user has already logged");
     }
-
-    $this->_document = new UserRegisterDocument(
-      $username, $password, $rePassword
-    );
   }
 
   /**
@@ -59,11 +56,11 @@ class UserRegisterController extends Controller
    */
   public function onPostRequest()
   {
-    $username = $this->_document->getUsername();
-    $password = $this->_document->getPassword();
-    $rePassword = $this->_document->getRePassword();
+    $this->_username = $this->getParam("username");
+    $password = $this->getParam("password");
+    $rePassword = $this->getParam("rePassword");
 
-    if (   Text::isEmpty($username)
+    if (   Text::isEmpty($this->_username)
         || Text::isEmpty($password)
         || Text::isEmpty($rePassword)
     ) {
@@ -72,11 +69,12 @@ class UserRegisterController extends Controller
       );
     }
 
-    $user = DbUser::searchByName($this->db, $username);
+    $user = DbUser::searchByName($this->db, $this->_username);
     if ($user !== null) {
       throw new ClientException("The user already exist");
     }
 
+    // TODO: move this code to DbUser
     if (strlen($password) < MIN_PASSWORD_LENGTH) {
       throw new ClientException(
         "Password must have at least " . MIN_PASSWORD_LENGTH . " characters"
@@ -87,7 +85,7 @@ class UserRegisterController extends Controller
       throw new ClientException("Passwords do not match");
     }
 
-    UserAuth::create($this->db, $username, $password);
-    UserAuth::login($this->db, $username, $password);
+    UserAuth::create($this->db, $this->_username, $password);
+    UserAuth::login($this->db, $this->_username, $password);
   }
 }
