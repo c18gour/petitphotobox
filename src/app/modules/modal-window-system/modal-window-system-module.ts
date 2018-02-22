cf4c2bc76e179fde9c3e4187a85e52cde6b5c45c
soloproyectos <gonzalo.chumillas@gmail.com>
2018-02-22 13:23:59 +0100

    translate module

--- src/app/modules/modal-window-system/modal-window-system-module.ts
+++ src/app/modules/modal-window-system/modal-window-system-module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { ModalWindowSystem } from './modal-window-system';
 import { ModalLoadingComponent } from './components/modal-loading/modal-loading-component';
@@ -7,9 +10,22 @@ import { ModalAlertComponent } from './components/modal-alert/modal-alert-compon
 import { ModalConfirmComponent } from './components/modal-confirm/modal-confirm-component';
 import { ModalErrorComponent } from './components/modal-error/modal-error-component';
 
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   declarations: [
     ModalLoadingComponent,
