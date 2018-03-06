050ef1df5de4bf4287560e47ee03bdc63fc9092c
soloproyectos <gonzalo.chumillas@gmail.com>
2018-03-06 11:32:49 +0100

    remove unused files

--- server/app/image.php
+++
@@ -1,9 +0,0 @@
-<?php
-header("Content-Type: image/jpeg");
-require_once "src/vendor/autoload.php";
-require_once "config.php";
-require_once "app-config.php";
-use petitphotobox\controllers\ImageController;
-
-$c = new ImageController();
-$c->processRequest();
